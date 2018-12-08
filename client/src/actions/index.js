be75d96cd41204c74d6ffa6928076fff8e6031c7
mythLabs <amitbisht744@gmail.com>
2018-12-09 02:32:31 +0530

    Added stream CRUD actions,types

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,20 +1,54 @@
-import {SIGN_IN, SIGN_OUT} from './types';
-import streams from '../apis/streams';
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
+import streams from "../apis/streams";
 
-export const SignIn = (userId) => {
-    return {
-        type: SIGN_IN,
-        payload: userId
-    }
-}
+export const SignIn = userId => {
+  return {
+    type: SIGN_IN,
+    payload: userId
+  };
+};
 
-export const SignOut = (userId) =>{
-    return {
-        type: SIGN_OUT,
-        payload: userId
-    }
-}
+export const SignOut = userId => {
+  return {
+    type: SIGN_OUT,
+    payload: userId
+  };
+};
 
-export const createStream = (formValues) => async (dispatch) => {
-   const response = await streams.post('/streams',formValues);
-}
+export const createStream = formValues => async dispatch => {
+  const response = await streams.post("/streams", formValues);
+
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+
+export const fetchStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: DELETE_STREAM, payload:id});
+};
+
+export const updateStream = (id,formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
