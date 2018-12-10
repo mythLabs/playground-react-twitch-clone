2d2bbbe9560bf1f7f00726d6b3565e23e949e1db
mythLabs <amitbisht744@gmail.com>
2018-12-10 14:18:16 +0530

    Added custom history object and chnage from browser history to history

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import history from '../history';
+
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -28,6 +30,7 @@ export const createStream = formValues => async (dispatch,getState) => {
   const response = await streams.post("/streams", {...formValues,userId});
   
   dispatch({ type: CREATE_STREAM, payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
