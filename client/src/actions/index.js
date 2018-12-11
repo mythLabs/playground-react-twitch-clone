68bc355c7d2d764b35105c7a37f0960c5fc67f48
mythLabs <amitbisht744@gmail.com>
2018-12-12 01:36:09 +0530

    navigate to root on deletion

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -49,6 +49,7 @@ export const deleteStream = id => async dispatch => {
   await streams.delete(`/streams/${id}`);
 
   dispatch({ type: DELETE_STREAM, payload:id});
+  history.push('/');
 };
 
 export const updateStream = (id,formValues) => async dispatch => {
