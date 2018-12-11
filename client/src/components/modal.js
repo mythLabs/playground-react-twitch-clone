c4a0542151e82d74c22867e1b1e42275cf16b3d3
mythLabs <amitbisht744@gmail.com>
2018-12-12 00:49:52 +0530

    Added Portal base for delete modal

---
+++ client/src/components/modal.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Modal = () => {
+  return ReactDOM.createPortal(
+    <div className="ui dimmer modals visible active" >
+      <div className="ui standard modal visible active">
+      dsfsdf
+      </div>
+    </div>,document.querySelector('#modal')
+  );
+};
+
+export default Modal;
