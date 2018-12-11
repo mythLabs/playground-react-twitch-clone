38f1c8fd58cf81f26dc47188d057aedd801927bd
mythLabs <amitbisht744@gmail.com>
2018-12-12 01:00:59 +0530

    Added stling to modal

--- client/src/components/modal.js
+++ client/src/components/modal.js
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from '../history';
 
 const Modal = () => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active" >
-      <div className="ui standard modal visible active">
-      dsfsdf
+    <div className="ui dimmer modals visible active" onClick={() => history.push('/')}>
+      <div className="ui standard modal visible active"  onClick={(e) => e.stopPropagation()}>
+        <div className="header">
+         Delete Stream
+        </div>
+        <div className="content">Are you sure you want to delete ?</div>
+          <div className="actions">
+            <button className="ui primary button">Delete</button>
+            <button className="ui button">Cancel</button>
+          </div>
       </div>
-    </div>,document.querySelector('#modal')
+    </div>,
+    document.querySelector("#modal")
   );
 };
 
