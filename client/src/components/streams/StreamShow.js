59092263df00209052ff85bfff0bd0f0c0cbbbb5
mythLabs <amitbisht744@gmail.com>
2018-12-12 15:35:40 +0530

    Added code to fetch flv stream from RTMP server into browser

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,41 @@
 import React from "react";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
+import flv from 'flv.js';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef =React.createRef();
+  }
+
   componentDidMount() {
-    debugger;
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if (this.player || !this.props.stream) {
+      return
+    }
+    const { id } = this.props.match.params;
+    this.player =flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${ id}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
@@ -15,12 +45,15 @@ class StreamShow extends React.Component {
     } else {
       return (
         <div>
+          <video ref={this.videoRef} style={{width:'100%'}} controls= {true}/>
           <h1>{stream.title}</h1> <h5>{stream.description}</h5>
         </div>
       );
     }
   }
 }
+
+
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
